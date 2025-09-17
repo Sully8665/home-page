048e936b42cfd763922268210982951a08529704
Saleh <saleh.rezaei@gmail.com>
2025-09-17 10:54:32 +0330

    vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, HostListener, ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  
-  constructor(private cdr: ChangeDetectorRef) {}
-  
+export class AppComponent
+{
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
   @HostListener('window:hashchange', ['$event'])
-  onHashChange(event: HashChangeEvent) {
+  onHashChange(event: HashChangeEvent)
+  {
     // Trigger change detection when hash changes
     this.cdr.detectChanges();
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
-import { SkillsComponent } from './pages/skills/skills.component';
-import { ExperienceComponent } from './pages/experience/experience.component';
-import { EducationComponent } from './pages/education/education.component';
-import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 import { SkillModalComponent } from './shared/components/skill-modal/skill-modal.component';
 import { MarkdownPipe } from './shared/pipes/markdown.pipe';
 
@@ -22,4 +22,4 @@ import { MarkdownPipe } from './shared/pipes/markdown.pipe';
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, MatIconModule],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
