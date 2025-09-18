47c45fa9f30ff5ba5593b5f8ae7f8cdbb8f778eb
Saleh <saleh.rezaei@gmail.com>
2025-09-18 07:06:18 +0330

    avatar style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,8 @@
-import { Component } from '@angular/core'; import { SUMMARY, SUMMARY_FA, ROLE, ROLE_FA, NAME, NAME_FA } from '../../shared/models/data'; import { LanguageService } from '../../shared/language.service';
-@Component({selector:'app-home', templateUrl:'./home.component.html'})
-export class HomeComponent{ constructor(public lang: LanguageService){} NAME=NAME; NAME_FA=NAME_FA; ROLE=ROLE; ROLE_FA=ROLE_FA; get summary(){ return this.lang.current==='fa'? SUMMARY_FA : SUMMARY; } }
+import { Component } from '@angular/core';
+import { LanguageService } from '../../shared/language.service';
+import { NAME, NAME_FA, ROLE, ROLE_FA, SUMMARY, SUMMARY_FA } from '../../shared/models/data';
+@Component({
+    selector: 'app-home', templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent { constructor(public lang: LanguageService) { } NAME = NAME; NAME_FA = NAME_FA; ROLE = ROLE; ROLE_FA = ROLE_FA; get summary() { return this.lang.current === 'fa' ? SUMMARY_FA : SUMMARY; } }
